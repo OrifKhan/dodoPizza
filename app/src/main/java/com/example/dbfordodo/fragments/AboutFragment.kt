9517f1ae01ce04983dd2632185e9c928b4f7cc7e
Husenoda Orifkhon <husenzodao@gmail.com>
2023-04-01 17:51:09 +0500

    set contanct

---
+++ app/src/main/java/com/example/dbfordodo/fragments/AboutFragment.kt
@@ -0,0 +1,31 @@
+package com.example.dbfordodo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.navigation.fragment.findNavController
+import com.example.dbfordodo.R
+
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val back = view.findViewById<LinearLayout>(R.id.backFragment0)
+        back.setOnClickListener {
+            val navController = findNavController()
+            navController.popBackStack()
+        }
+    }
+
+}
